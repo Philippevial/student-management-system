cecf2ae9d26a447fc4ead8b0e41dc5f097bec5d8
Philippe <philippe.vial90@gmail.com>
2022-02-23 12:29:41 +0100

    Refactor code

--- src/main/java/se/iths/error/ErrorMessage.java
+++ src/main/java/se/iths/error/ErrorMessage.java
@@ -1,18 +1,23 @@
-package se.iths.errors;
+package se.iths.error;
 
 import java.time.LocalDateTime;
 
 public class ErrorMessage {
 
     LocalDateTime localDateTime = LocalDateTime.now();
-    String errorCode;
-    String message;
-    String url;
+    private final String errorCode;
+    private final String message;
+    private final String url;
+
 
     public ErrorMessage(String errorCode, String message, String url) {
         this.errorCode = errorCode;
         this.message = message;
         this.url = url;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
     }
 
     public String getErrorCode() {
