cde1c47e226a599fb66b785928861173a915ca1e
Philippe <philippe.vial90@gmail.com>
2022-02-23 12:28:52 +0100

    Add Exception classes

---
+++ src/main/java/se/iths/exceptions/BadRequestException.java
@@ -0,0 +1,16 @@
+package se.iths.exceptions;
+
+import se.iths.error.ErrorMessage;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class BadRequestException extends WebApplicationException {
+    public BadRequestException(ErrorMessage errorMessage) {
+        super(Response.status(Response.Status.BAD_REQUEST)
+                .entity(errorMessage)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .build());
+    }
+}
